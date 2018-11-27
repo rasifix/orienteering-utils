f6dd4331fc67c083790a68181180f38421d241c4
Simon Raess <cocoa@gmx.ch>
2018-11-28 00:07:56 +0100

    initial dump of some library code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Description
+Library functions for orienteering split analyzis. Currently supported file 
+formats are Kraemer and Oware.
---
+++ index.js
@@ -0,0 +1,7 @@
+import oware from './lib/oware';
+import kraemer from './lib/kraemer';
+
+module.exports = {
+    oware: oware,
+    kraemer: kraemer
+};
